2fd32fbed0ab4e5ce722a81ff18b9d8078f9fbe0
Nathan Johnson <nathancj224@gmail.com>
2018-07-17 10:38:40 -0400

    Created base button component, added colors constants file"

--- js/screens/HomeScreen.js
+++ js/screens/HomeScreen.js
@@ -1,47 +1,55 @@
 import React from 'react';
 
 import {
-    Text,
-    View,
-    Button,
-    StyleSheet
+  Text,
+  View,
+  StyleSheet
 } from 'react-native';
 
+import BaseButton from '../components/BaseButton';
+import colors from '../styles/colors';
+
+function Button(props) {
+  return <BaseButton color="#fff" androidTextColor={colors.primaryColor} {...props} />;
+}
+
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-        header: null
-    };
-    goToLogin = () => this.props.navigation.navigate('Login');
-    goToRegister = () => this.props.navigation.navigate('Register');
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.headerText}>Welcome to MyChat!</Text>
-                <View style={styles.buttonContainer}>
-                    <Button onPress={this.goToLogin} title="Sign In" />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button onPress={this.goToRegister} title="Create an Account" />
-                </View>
-            </View>
-        );
-    }
+  static navigationOptions = {
+    header: null
+  };
+  goToLogin = () => this.props.navigation.navigate('Login');
+  goToRegister = () => this.props.navigation.navigate('Register');
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Welcome to MyChat!</Text>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.goToLogin} title="Sign In" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.goToRegister} title="Create an Account" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 25,
-    },
-    headerText:{
-        fontSize: 30,
-        textAlign: 'center',
-        marginBottom: 50
-    },
-    buttonContainer: {
-        marginBottom: 20,
-        width: '70%',
-        alignSelf: 'center'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 25,
+    backgroundColor: colors.primaryColor,
+  },
+  headerText:{
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 50,
+    color: '#fff',
+  },
+  buttonContainer: {
+    marginBottom: 20,
+    width: '70%',
+    alignSelf: 'center'
+  }
 });
---
+++ js/styles/colors.js
@@ -0,0 +1,4 @@
+export default {
+  primaryColor: '#0B5DA1',
+  secondaryColor: '#EB9710'
+}
